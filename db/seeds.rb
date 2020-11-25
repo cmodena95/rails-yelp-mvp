19b12791d60403f34d82d42643f91e6cebae86a5
Camila Modena <itscamilla@hotmail.com>
2020-11-25 15:41:30 +0000

    Worked on Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+
+Restaurant.destroy_all
+
+puts 'Generating restaurants...'
+
+10.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  number = Faker::PhoneNumber.phone_number
+  category = ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  restaurant = Restaurant.create(name: name, address: address, phone_number: number, category: category)
+  restaurant.save
+end
+
+puts 'Finished!'
+
